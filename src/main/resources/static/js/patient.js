a690a2b1b0a6f60c81d58cc59b9d291dbcaf269b
Ditrim <ditrim020@ukr.net>
2018-07-08 17:09:22 +0300

    New front-end.Add patient

--- src/main/resources/static/js/patient.js
+++ src/main/resources/static/js/patient.js
@@ -1,7 +1,37 @@
-function check() {
-    var j = document.getElementsByClassName("inn");
-    if(j==""){
-        /*document.write("this field must not be empty")*/
-        alert("error")
+$('#patient-form').validate({
+    rules:{
+        name:{
+            required:true
+        },
+        surname:{
+            required:true
+        },
+        dateBirth:{
+            required:true
+        },
+        doctorId:{
+            required:true,
+        }
+    },
+    messages:{
+        name:{
+            required:'Must not be empty'
+        },
+        surname:{
+            required:'Must not be empty'
+        },
+        dateBirth:{
+            required:'Must not be empty'
+        },
+        doctorId:{
+            required:'Must not be empty'
+        }
+    },
+    errorPlacement: function (error, element) {
+        if (element.attr('type') == 'radio') {
+            error.insertBefore(element.parent());
+        } else {
+            error.insertBefore(element);
+        }
     }
-}
+});
