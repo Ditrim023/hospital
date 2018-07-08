dbd6b4aee473e704a12f59c08b71b097aa61ae73
Ditrim <ditrim020@ukr.net>
2018-07-08 16:41:33 +0300

    Add new front-end.Style and new profile

--- src/main/resources/static/js/log.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-     var j = document.getElementById('position').value;
-    var out = document.getElementById('bar');
-    if(position.innerHTML!="Admin"){
-        out.outerHTML = ' <h2>Hello Doctor<h2>';
-    }
-}
