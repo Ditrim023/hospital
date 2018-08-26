4cb41a2595c45e8c10c315625751a3a3e0425864
Ditrim <ditrim020@ukr.net>
2018-08-26 18:55:50 +0300

    disable opportunity change Sent from

--- src/main/java/com/hospital/security/ProfileConfig.java
+++ src/main/java/com/hospital/security/ProfileConfig.java
@@ -32,7 +32,7 @@ public class ProfileConfig extends WebSecurityConfigurerAdapter {
                 authorizeRequests()
                 .antMatchers("/index").permitAll()
                 .antMatchers("/profile").hasAnyRole("ADMIN", "DOCTOR")
-                .antMatchers("/patient/list", "/activity").hasRole("ADMIN")
+                .antMatchers("/patient/list", "/activity").hasAnyRole("ADMIN", "DOCTOR")
                 .and().exceptionHandling().accessDeniedPage("/403")
                 .and().formLogin()
                 .loginPage("/login").failureUrl("/403")
